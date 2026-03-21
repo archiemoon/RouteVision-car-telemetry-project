2bb100b90bf652a0fc2785a8253e9177b4dfc39a
am05422 <am05422@surrey.ac.uk>
2026-03-21 11:49:23 +0000

    added log for geolocate as not working

--- www/app.js
+++ www/app.js
@@ -1265,6 +1265,7 @@ setHomeBtn.addEventListener("click", async () => {
             latitude: coords.latitude,
             longitude: coords.longitude
         };
+        console.log(location);
         await Preferences.set({ key: "location", value: JSON.stringify(location) });
         await updateFuelPrice();
         await refreshPages();
